93a608df8ed52cdfaafd6873eba35cbb4568f043
julian hurtado <desarrollador.ti3@gmail.com>
2024-02-09 14:30:28 -0500

    corrección del código

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   PORT: 3000,
-  directorioDestino: "/app/", // Agrega tu directorioDestino aquí
+  directorioDestino: "/app/", // Ruta del directorio de destino en el contenedor
   nombreArchivo: "archivo_creado.txt",
   // Agrega otras variables de configuración según sea necesario
 };
